723b9ad480aa05e6169bcc58708275c820c6ee64
Shawn Lee <slee1271@mtroyal.ca>
2024-10-24 14:10:24 -0600

    radio buttons

--- radiobuttons.py
+++ radiobuttons.py
@@ -1,5 +1,5 @@
 import sys 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QRadioButton, QButtonGroup
 from PyQt5.QtGui import QIcon
 
 class MainWindow(QMainWindow):
@@ -8,6 +8,45 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My first GUI")
         self.setGeometry(700,250,500,500) # (x,y,width,height)
         self.setWindowIcon(QIcon("Monkey.png"))
+        self.radio1 = QRadioButton("Visa", self)
+        self.radio2 = QRadioButton("Mastercard", self)
+        self.radio3 = QRadioButton("Gift Card", self)
+        self.radio4 = QRadioButton("In-Store", self)
+        self.radio5 = QRadioButton("Online", self)
+        self.button_group1 = QButtonGroup(self)
+        self.button_group2 = QButtonGroup(self)
+
+        self.initUI()
+
+    def initUI(self):
+        self.radio1.setGeometry(0,0,300,50)
+        self.radio2.setGeometry(0,50,300,50)
+        self.radio3.setGeometry(0,100,300,50)
+        self.radio4.setGeometry(0,150,300,50)
+        self.radio5.setGeometry(0,200,300,50)
+
+        self.setStyleSheet("QRadioButton{"
+                           "font-size: 40px;}"
+                           "font-family: Arial;"
+                           "padding: 10px"
+                           "}")
+        self.button_group1.addButton(self.radio1)
+        self.button_group1.addButton(self.radio2)
+        self.button_group1.addButton(self.radio3)
+        self.button_group2.addButton(self.radio4)
+        self.button_group2.addButton(self.radio5)
+
+        self.radio1.toggled.connect(self.radio_button_changed)
+        self.radio2.toggled.connect(self.radio_button_changed)
+        self.radio3.toggled.connect(self.radio_button_changed)
+        self.radio4.toggled.connect(self.radio_button_changed)
+        self.radio5.toggled.connect(self.radio_button_changed)
+
+    def radio_button_changed(self):
+        radio_button = self.sender()
+        if radio_button.isChecked():
+            print(f"{radio_button.text()} is selected")
+
 
 def main():
     app = QApplication(sys.argv) 
